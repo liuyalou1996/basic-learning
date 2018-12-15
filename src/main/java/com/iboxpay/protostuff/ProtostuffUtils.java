d62449e68375713579ead225ecd5d1087c91763c
liuyalou <liuyalou@iboxpay.com>
2018-12-15 23:15:39 +0800

    添加ProtostuffUtils工具类以及Ognl使用示例

--- src/main/java/com/iboxpay/protostuff/ProtostuffUtils.java
+++ src/main/java/com/iboxpay/protostuff/ProtostuffUtils.java
@@ -1,5 +1,8 @@
 package com.iboxpay.protostuff;
 
+import java.util.List;
+import java.util.Map;
+
 import io.protostuff.LinkedBuffer;
 import io.protostuff.ProtostuffIOUtil;
 import io.protostuff.Schema;
@@ -34,4 +37,40 @@ public class ProtostuffUtils {
     ProtostuffIOUtil.mergeFrom(data, message, schema);
     return message;
   }
+
+  /**
+   * 包装集合的POJO，由于Protostuff获取模式时指定的运行时对象不能是接口或者抽象类的运行时对象，因此序列化集合时需要通过POJO来包装
+   * <p>
+   * @author liuyalou
+   * @since 2018年12月15日
+   * <p>
+   */
+  public static class CollectionWrapper<T> {
+
+    private List<T> list;
+
+    private Map<String, T> map;
+
+    public List<T> getList() {
+      return list;
+    }
+
+    public void setList(List<T> list) {
+      this.list = list;
+    }
+
+    public Map<String, T> getMap() {
+      return map;
+    }
+
+    public void setMap(Map<String, T> map) {
+      this.map = map;
+    }
+
+    @Override
+    public String toString() {
+      return "CollectionWrapper [list=" + list + ", map=" + map + "]";
+    }
+  }
+
 }

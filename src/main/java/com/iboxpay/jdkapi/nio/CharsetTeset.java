2ce756e9900fdfa878527a06a7d3dd1e2266c7bb
liuyalou <liuyalou@iboxpay.com>
2019-01-29 18:09:03 +0800

    添加nio2相关示例

---
+++ src/main/java/com/iboxpay/jdkapi/nio/CharsetTeset.java
@@ -0,0 +1,30 @@
+package com.iboxpay.jdkapi.nio;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class CharsetTeset {
+
+  public static void main(String[] args) throws Exception {
+    Charset charset = StandardCharsets.UTF_8;
+    CharsetEncoder encoder = charset.newEncoder();
+    CharsetDecoder decoder = charset.newDecoder();
+
+    CharBuffer cbuffer = CharBuffer.allocate(8);
+    cbuffer.put('a');
+    cbuffer.put('b');
+    cbuffer.put('c');
+    cbuffer.flip();
+
+    ByteBuffer bbuffer = encoder.encode(cbuffer);
+    for (int i = 0; i < bbuffer.capacity(); i++) {
+      System.out.print(bbuffer.get(i) + " ");
+    }
+    System.out.println("\n" + decoder.decode(bbuffer).toString());
+  }
+
+}
---
+++ src/main/java/com/iboxpay/jdkapi/nio2/FileVisitorTest.java
@@ -0,0 +1,37 @@
+package com.iboxpay.jdkapi.nio2;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class FileVisitorTest {
+
+  public static void main(String[] args) throws Exception {
+    SimpleFileVisitor<Path> visitor = new SimpleFileVisitor<Path>() {
+
+      @Override
+      public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        System.out.println("正在访问目录：" + dir.toAbsolutePath());
+        return FileVisitResult.CONTINUE;
+      }
+
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println("正在访问文件：" + file.toAbsolutePath());
+        if (file.endsWith("FileVisitorTest.java")) {
+          return FileVisitResult.TERMINATE;
+        }
+        return FileVisitResult.CONTINUE;
+      }
+
+    };
+
+    Path path = Paths.get("");
+    Files.walkFileTree(path, visitor);
+  }
+
+}

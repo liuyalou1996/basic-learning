53bf7b7edb214eff812b8937f3a6f277348a2540
liuyalou <liuyalou@iboxpay.com>
2019-01-30 14:00:03 +0800

    NIO2文件属性视图示例

---
+++ src/main/java/com/iboxpay/jdkapi/jdk8/datetime/DateUtils.java
@@ -0,0 +1,40 @@
+package com.iboxpay.jdkapi.jdk8.datetime;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class DateUtils {
+
+  public static final String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
+  public static final String DATE_FORMAT = "yyyy-MM-dd";
+  public static final String DATE_TIME_MILLS_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
+
+  private static DateFormat df = new SimpleDateFormat(DEFAULT_FORMAT);
+
+  public static Date parseStringToDate(String dateStr) throws ParseException {
+    return parseStringToDate(dateStr, null);
+  }
+
+  public static Date parseStringToDate(String dateStr, String format) throws ParseException {
+    if (StringUtils.isEmpty(format)) {
+      df = new SimpleDateFormat(format);
+    }
+    return df.parse(dateStr);
+  }
+
+  public static String parseDateToString(Date date) {
+    return parseDateToString(date);
+  }
+
+  public static String parseDateToString(Date date, String format) {
+    if (StringUtils.isEmpty(format)) {
+      df = new SimpleDateFormat(format);
+    }
+    return df.format(date);
+  }
+
+}
---
+++ src/main/java/com/iboxpay/jdkapi/nio2/FileAttributeViewTest.java
@@ -0,0 +1,55 @@
+package com.iboxpay.jdkapi.nio2;
+
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.DosFileAttributeView;
+import java.nio.file.attribute.FileOwnerAttributeView;
+import java.nio.file.attribute.UserPrincipal;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FileAttributeViewTest {
+
+  private static final DateFormat df = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒");
+
+  public static void main(String[] args) throws Exception {
+    File file = new File(FileAttributeViewTest.class.getResource("").getPath());
+    Path path = Paths.get(file.getAbsolutePath(), "AttributeViewTest.class");
+
+    // 获取文件基本信息
+    BasicFileAttributeView basicView = Files.getFileAttributeView(path, BasicFileAttributeView.class);
+    BasicFileAttributes basicAttrs = basicView.readAttributes();
+
+    Date creationTime = new Date(basicAttrs.creationTime().toMillis());
+    Date lastAccessTime = new Date(basicAttrs.lastAccessTime().toMillis());
+    Date lastModifiedTime = new Date(basicAttrs.lastModifiedTime().toMillis());
+    long size = basicAttrs.size() / 1024;
+    System.out.println("文件创建时间为：" + df.format(creationTime));
+    System.out.println("文件最后访问时间为：" + df.format(lastAccessTime));
+    System.out.println("文件最后修改时间为：" + df.format(lastModifiedTime));
+    System.out.println("文件大小为：" + size + "kb");
+
+    // 获取文件属主信息
+    FileOwnerAttributeView ownerView = Files.getFileAttributeView(path, FileOwnerAttributeView.class);
+    UserPrincipal principal = ownerView.getOwner();
+    System.out.println("文件的拥有者为：" + principal);
+
+    UserPrincipal prin = FileSystems.getDefault().getUserPrincipalLookupService().lookupPrincipalByName("liuyalou");
+    System.out.println(prin.getName());
+
+    path = Paths.get(System.getProperty("user.home"), "hidden.txt");
+    // 获取访问dos属性的视图
+    DosFileAttributeView dosView = Files.getFileAttributeView(path, DosFileAttributeView.class);
+    // 设为隐藏
+    dosView.setReadOnly(true);
+    // 设为只读
+    dosView.setHidden(true);
+  }
+
+}

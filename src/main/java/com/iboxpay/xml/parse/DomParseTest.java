9497734158777d6516e943fb0d519f888f5a2615
liuyalou <liuyalou@iboxpay.com>
2019-02-13 11:01:14 +0800

    添加DOM解析和DOM4j解析示例

---
+++ src/main/java/com/iboxpay/xml/parse/DomParseTest.java
@@ -0,0 +1,43 @@
+package com.iboxpay.xml.parse;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import com.iboxpay.xml.entity.User;
+
+public class DomParseTest {
+
+  public static void main(String[] args) throws Exception {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder builder = factory.newDocumentBuilder();
+    File file = new File(DomParseTest.class.getResource("/").getFile(), "user.xml");
+
+    Document doc = builder.parse(file);
+    Element ele = doc.getDocumentElement();
+    NodeList nodeList = ele.getElementsByTagName("user");
+
+    List<User> userList = new ArrayList<>();
+    for (int count = 0; count < nodeList.getLength(); count++) {
+      User user = new User();
+      Element sub = (Element) nodeList.item(count);
+      int id = Integer.parseInt(sub.getAttribute("id"));
+      String name = sub.getElementsByTagName("name").item(0).getFirstChild().getTextContent();
+      String password = sub.getElementsByTagName("password").item(0).getFirstChild().getTextContent();
+      user.setId(id);
+      user.setName(name);
+      user.setPassword(password);
+      userList.add(user);
+    }
+
+    System.out.println(userList);
+  }
+
+}

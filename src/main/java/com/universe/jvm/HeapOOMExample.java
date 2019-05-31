42d3f3f05e860a2c3a8292ffced66637ce09a41c
liuyalou <liuyalou@iboxpay.com>
2019-05-31 18:23:16 +0800

    jvm OOM简单分析示例

---
+++ src/main/java/com/universe/jvm/HeapOOMExample.java
@@ -0,0 +1,22 @@
+package com.universe.jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM Args:-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * 将Java堆大小设为20m，官方推荐初始堆和最大堆大小保持一致，HeapDumpOnOutOfMemoryError参数用来导出错误信息
+ */
+public class HeapOOMExample {
+
+	static class OOMObject {
+
+	}
+
+	public static void main(String[] args) {
+		List<OOMObject> list = new ArrayList<>();
+		while (true) {
+			list.add(new OOMObject());
+		}
+	}
+}

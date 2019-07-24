06031ca1f9891fc2576591f013d5e245eabe0067
liuyalou <liuyalou@iboxpay.com>
2019-07-24 16:34:01 +0800

    GsonUtils工具类优化

--- src/main/java/com/universe/thirdparty/gson/GsonUtils.java
+++ src/main/java/com/universe/thirdparty/gson/GsonUtils.java
@@ -12,21 +12,24 @@
  */
 package com.universe.thirdparty.gson;
 
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.commons.lang3.StringUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
+import com.google.gson.TypeAdapter;
+import com.google.gson.internal.LinkedTreeMap;
 import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
 
 /**
  * @author: liuyalou
@@ -34,10 +37,17 @@ import com.google.gson.reflect.TypeToken;
  */
 public abstract class GsonUtils {
 
+  public static final Type MAP_TYPE = new TypeToken<Map<String, Object>>() {}.getType();
+  public static final Type LIST_TYPE = new TypeToken<List<Map<String, Object>>>() {}.getType();
+
+  private static final CollectionTypeAdapter COLLECTION_TYPE_ADAPTER = new CollectionTypeAdapter();
+
+  private static final Map<Type, Gson> GSON_INSTANCE_CACHE = new ConcurrentHashMap<>();
+
   private static final Gson DEFAULT_GSON = new Gson();
 
   public static String toJsonString(Object obj) {
-    return DEFAULT_GSON.toJson(obj);
+    return Objects.isNull(obj) ? null : DEFAULT_GSON.toJson(obj);
   }
 
   public static String toPrettyJsonString(Object obj) {
@@ -49,13 +59,20 @@ public abstract class GsonUtils {
   }
 
   public static String toPrettyJsonStringWithNullValue(Object obj) {
-    return toJsonString(obj, false, false, true);
+    return toJsonString(obj, true, true, false);
   }
 
   public static String toJsonStringWithExcludeMode(Object obj) {
     return toJsonString(obj, false, false, true);
   }
 
+  /**
+   * @param obj
+   * @param serializeNulls 是否序列化空值
+   * @param prettyFormat  是否美化打印
+   * @param excludeField 是否过滤属性，不带@Expose注解的属性将不会被序列化和反序列化
+   * @return
+   */
   private static String toJsonString(Object obj, boolean serializeNulls, boolean prettyFormat, boolean excludeField) {
     if (Objects.isNull(obj)) {
       return null;
@@ -74,49 +91,108 @@ public abstract class GsonUtils {
     if (excludeField) {
       builder.excludeFieldsWithoutExposeAnnotation();
     }
+
     Gson gson = builder.create();
     return gson.toJson(obj);
   }
 
-  public static <T> T toJavaBean(String jsonStr, Class<T> clazz) {
-    return DEFAULT_GSON.fromJson(jsonStr, clazz);
-  }
-
-  public static <T> T toCollection(String jsonStr, TypeToken<T> typeToken) {
-    return DEFAULT_GSON.fromJson(jsonStr, typeToken.getType());
-  }
-
-  public static class JsonToMapDeserializer implements JsonDeserializer<Map<String, Object>> {
-
-    @Override
-    public Map<String, Object> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-        throws JsonParseException {
-      JsonObject jo = json.getAsJsonObject();
+  public static <T> T toBean(String jsonStr, Class<T> clazz) {
+    if (StringUtils.isEmpty(jsonStr)) {
       return null;
     }
 
+    return DEFAULT_GSON.fromJson(jsonStr, clazz);
   }
 
-  public static void main(String[] args) {
-    Map<String, Object> map = new HashMap<>();
-    map.put("name", "liuyalou");
-    map.put("age", 20);
+  public static <T> T toCollection(String jsonStr, Type type) {
+    if (StringUtils.isEmpty(jsonStr)) {
+      return null;
+    }
 
-    List<Map<String, Object>> list = new ArrayList<>();
-    list.add(map);
-
-    String jsonStr = toJsonString(map);
-    System.out.println(jsonStr);
-
-    System.out.println(toCollection(jsonStr, new TypeToken<Map<String, Object>>() {}));
-
-    GsonBuilder builder = new GsonBuilder();
-    Type type = new TypeToken<Map<String, Object>>() {}.getType();
-    builder.registerTypeAdapter(type, new JsonToMapDeserializer());
-    Gson gson = builder.create();
-
-    Map<String, Object> newMap = gson.fromJson(jsonStr, type);
-    System.out.println(newMap);
-
+    Gson gson = getGsonInstance(type);
+    return gson.fromJson(jsonStr, type);
   }
+
+  public static <T> T mapToBean(Map<String, ? extends Object> map, Class<T> clazz) {
+    return toBean(toJsonString(map), clazz);
+  }
+
+  public static Map<String, Object> beanToMap(Object obj) {
+    return toCollection(toJsonString(obj), MAP_TYPE);
+  }
+
+  private static Gson getGsonInstance(Type type) {
+    Gson gson = GSON_INSTANCE_CACHE.get(type);
+    if (Objects.isNull(gson)) {
+      GsonBuilder builder = new GsonBuilder();
+
+      if (MAP_TYPE.equals(type)) {
+        // 反序列化为Map<String,Object>时处理数字需要
+        builder.registerTypeAdapter(MAP_TYPE, COLLECTION_TYPE_ADAPTER);
+      } else if (LIST_TYPE.equals(type)) {
+        // 反序列化为List<Map<String,Object>>时处理数字需要
+        builder.registerTypeAdapter(LIST_TYPE, COLLECTION_TYPE_ADAPTER);
+      }
+
+      gson = builder.create();
+      GSON_INSTANCE_CACHE.put(type, gson);
+    }
+
+    return gson;
+  }
+
+  private static class CollectionTypeAdapter extends TypeAdapter<Object> {
+
+    @Override
+    public Object read(JsonReader in) throws IOException {
+      JsonToken token = in.peek();
+      switch (token) {
+        case BEGIN_ARRAY:
+          List<Object> list = new ArrayList<Object>();
+          in.beginArray();
+          while (in.hasNext()) {
+            list.add(read(in));
+          }
+          in.endArray();
+          return list;
+
+        case BEGIN_OBJECT:
+          Map<String, Object> map = new LinkedTreeMap<String, Object>();
+          in.beginObject();
+          while (in.hasNext()) {
+            map.put(in.nextName(), read(in));
+          }
+          in.endObject();
+          return map;
+
+        case STRING:
+          return in.nextString();
+
+        case NUMBER:
+          // 改写数字处理逻辑
+          String numberStr = in.nextString();
+          if (numberStr.contains(".") || numberStr.contains("e") || numberStr.contains("E")) {
+            return Double.parseDouble(numberStr);
+          }
+
+          long number = Long.parseLong(numberStr);
+          return number <= Integer.MAX_VALUE ? Integer.parseInt(numberStr) : number;
+
+        case BOOLEAN:
+          return in.nextBoolean();
+
+        case NULL:
+          in.nextNull();
+          return null;
+
+        default:
+          throw new IllegalStateException();
+      }
+    }
+
+    @Override
+    public void write(JsonWriter out, Object value) throws IOException {
+    }
+  }
+
 }

9b1f8fdcfd8f31cf7eafea02be11e107fc771732
Nick Liu <2462405721@qq.com>
2022-09-22 16:04:42 +0800

    二维码生成工具类修改

--- src/main/java/com/universe/thirdparty/zxing/QrCodeUtils.java
+++ src/main/java/com/universe/thirdparty/zxing/QrCodeUtils.java
@@ -2,24 +2,16 @@ package com.universe.thirdparty.zxing;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
-import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageConfig;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-import org.apache.commons.io.FileUtils;
 
-import javax.activation.MimeType;
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
@@ -35,6 +27,9 @@ public abstract class QrCodeUtils {
 	 */
 	private static final String DEFAULT_FORMAT = "png";
 
+	/**
+	 * 默认额外参数：可设置字符集、容错级别
+	 */
 	private static final Map<EncodeHintType, Object> DEFAULT_HINTS = new HashMap<>();
 
 	static {
@@ -42,12 +37,44 @@ public abstract class QrCodeUtils {
 		DEFAULT_HINTS.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
 	}
 
+	/**
+	 * 生成绿码
+	 * @param content 二维码展示内容
+	 * @param width 二维码图片宽度
+	 * @param height 二维码图片高度
+	 * @return 图片二进制流
+	 * @throws Exception
+	 */
+	public static byte[] generateGreenQrCode(String content, int width, int height) throws Exception {
+		return generateQrCode(content, width, height, Color.GREEN.getRGB(), Color.WHITE.getRGB());
+	}
+
+	/**
+	 * 生成二维码并转为二进制流
+	 * @param content 二维码展示内容
+	 * @param width 二维码图片宽度
+	 * @param height 二维码图片高度
+	 * @param onColorAsRGB 二维码数据图案颜色
+	 * @param offColorAsRGB 二维码背景色
+	 * @return 图片二进制流
+	 * @throws Exception
+	 */
 	public static byte[] generateQrCode(String content, int width, int height, int onColorAsRGB, int offColorAsRGB) throws Exception {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		generateQrCode(baos, content, width, height, onColorAsRGB, offColorAsRGB);
 		return baos.toByteArray();
 	}
 
+	/**
+	 * 生成二维码
+	 * @param os 输出流
+	 * @param content 二维码展示内容
+	 * @param width 二维码图片宽度
+	 * @param height 二维码图片高度
+	 * @param onColorAsRGB 二维码数据图案颜色
+	 * @param offColorAsRGB 二维码背景色
+	 * @throws Exception
+	 */
 	public static void generateQrCode(OutputStream os, String content, int width, int height, int onColorAsRGB, int offColorAsRGB) throws Exception {
 		QRCodeWriter qrCodeWriter = new QRCodeWriter();
 		BitMatrix bitMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, width, height, DEFAULT_HINTS);
@@ -55,6 +82,16 @@ public abstract class QrCodeUtils {
 		MatrixToImageWriter.writeToStream(bitMatrix, DEFAULT_FORMAT, os, matrixToImageConfig);
 	}
 
+	/**
+	 * 生成二维码
+	 * @param path 文件路径
+	 * @param content 二维码展示内容
+	 * @param width 二维码图片宽度
+	 * @param height 二维码图片高度
+	 * @param onColorAsRGB 二维码数据图案颜色
+	 * @param offColorAsRGB 二维码背景色
+	 * @throws Exception
+	 */
 	public static void generateQrCode(String path, String content, int width, int height, int onColorAsRGB, int offColorAsRGB) throws Exception {
 		QRCodeWriter qrCodeWriter = new QRCodeWriter();
 		BitMatrix bitMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, width, height, DEFAULT_HINTS);
